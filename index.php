9f4be5881e68d048ef51ea00af61851d02dc0e32
Hazel Charmagne Niza <charmhaze21@gmail.com>
2014-12-11 07:17:39 +0800

    edited path

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
 		<div class="container">
 			<div class="row title text-center">
-				<img src="images/title.png"/>
+				<img src="http://hushtones.gopagoda.com/images/title.png"/>
 			</div>
 			<div class="row tagline text-center">
 				"The right tone settings at the right place and at the right time."
